174fe5f4085a80fe53ffc1635398fb9dd8418152
rogerinnl <rogerlucas240@gmail.com>
2026-04-19 22:41:13 -0300

    add

--- Beecrowd/1165.c
+++ Beecrowd/1165.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+int verifica(int x){
+    if(x == 1){
+        return 1;
+    }
+    int count=0;
+    for(int i=1;i<=x;i++){
+        if(x % i == 0){
+            count++;
+        }
+    }
+    if(count == 2){
+        return 1;
+    } else {
+        return 0;
+    }
+}
 int main(){
-    int n;
+    int n,x;
     scanf("%d",&n);
-    for(int i;i<n)
+    for(int i=0;i<n;i++){
+        scanf("%d",&x);
+        if(verifica(x)){
+            printf("%d eh primo\n",x);
+        } else {
+            printf("%d nao eh primo\n",x);
+        }
+    }
     return 0;
 }
